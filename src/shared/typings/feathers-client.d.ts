f2496624308a51c169b843e27b95326fd3744472
Tinh Ngo <tiengtinh@gmail.com>
2016-06-26 14:14:55 +0700

    update gitignore and add typings

---
+++ src/shared/typings/feathers-client.d.ts
@@ -0,0 +1,20 @@
+declare var generic: any
+declare module 'feathers/client' {
+  export = generic
+}
+
+declare module 'feathers-hooks' {
+  export = generic
+}
+
+declare module 'feathers-socketio/client' {
+  export = generic
+}
+
+declare module 'feathers-authentication/client' {
+  export = generic
+}
+
+declare module 'socket.io-client' {
+  export = generic
+}
