332f5eb88d52f49cce5e3ffca75febd002a4e8a8
lam.khuong <lam.khuong@tripolis.com>
2016-07-03 13:13:27 +0700

    finish dropdown working with search bar

--- src/shared/routes/home.js
+++ src/shared/routes/home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropertyItem from '../components/PropertyItem/PropertyItem'
 import Block from '../components/Block/Block'
+import Dropdown from '../components/Dropdown/Dropdown'
 
 const s = require('./home.less')
 
@@ -11,25 +12,33 @@ class Home extends React.Component {
         <Block title='I want to'>
           <div className={'row'}>
             <div className={'col-md-2'} >
-            1
+              <Dropdown title='Buy'/>
+            </div>
+            <div className={'col-md-8'} >
+              <Dropdown title='Buy'/>
+            </div>
+          </div>
+          <div className={'row'}>
+            <div className={'col-md-2'} >
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-            1
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-           1
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-            1
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-            1
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-            1
+              <Dropdown title='Buy'/>
             </div>
             <div className={'col-md-1'} >
-            1
+              <Dropdown title='Buy'/>
             </div>
           </div>
         </Block>
