41a7a42acd81c9169705e1a4bb2780dac151ef18
lamkn <lam.khuong@tripolis.com>
2016-07-09 12:19:05 +0700

    add contact page

--- src/shared/routes/index.js
+++ src/shared/routes/index.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router'
 import App from '../containers/app'
 import Home from './home'
 import About from './about'
+import Contact from './contact'
 import PropertyDetail from './admin/PropertyDetail/PropertyDetail'
 
 require('font-awesome/css/font-awesome.css')
@@ -12,6 +13,7 @@ const routes = (
   <Route path='/' component={ App } >
     <IndexRoute component={ Home } />
     <Route path='about' component={ About } />
+    <Route path='contact' component={ Contact } />
     <Route path='admin/properties/:id' component={ PropertyDetail } />
   </Route>
 );
