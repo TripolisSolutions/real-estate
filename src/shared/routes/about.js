01c0fd7b794ad5e79c3bd77de8aae8c8c9a57e47
lam.khuong <lam.khuong@tripolis.com>
2016-07-11 13:48:26 +0700

    add simplae about us

--- src/shared/routes/about.js
+++ src/shared/routes/about.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Col as BootstrapCol} from 'react-bootstrap'
+import classnames from 'classnames'
 
-import { observer } from '../context'
 import Map from '../components/Map/Map'
+import Block from '../components/Block/Block'
 
-@observer
+const s = require('./about.less')
+
 class About extends React.Component {
 
   static fetchData(store) {
@@ -13,6 +16,25 @@ class About extends React.Component {
   render() {
     return (
       <div>
+        <div className={ 'container' } >
+          <Block title={ 'About us' } bigger ={ true }>
+            <div className={ classnames(s.img, 'row' ) } >
+              <BootstrapCol md={6}>
+                  <h2>Sonia Phương Trân</h2>
+                  <img src="http://www.incrediblethings.com/wp-content/uploads/2012/12/Alexa-Meade-Milk-Paintings-1.jpg" />
+              </BootstrapCol>
+              <BootstrapCol md={6}>
+                  <h2>Dean Walkerden</h2>
+                  <img src="http://4.bp.blogspot.com/-IrNv1zLJw-0/UIbGDQXXsmI/AAAAAAAAAGU/DjxOORCM0Lg/s1600/emma-hack_Car_shape_human_body_painting_interesting_art_sculpture.jpg" />
+              </BootstrapCol>
+            </div>
+            <div className= { s.text }>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in eleifend ipsum. Duis hendrerit turpis et sapien hendrerit, et convallis ligula ultrices. Integer venenatis venenatis neque non feugiat. Maecenas pretium nisi a pharetra malesuada. Sed bibendum lorem eu elit mattis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed fermentum dapibus nibh, eget commodo ante porttitor ut. Suspendisse pulvinar, magna id pulvinar mattis, tellus purus faucibus lorem, non porttitor neque massa quis odio. Duis lobortis suscipit nunc, id consectetur nisl vestibulum at. Curabitur id dui lacinia, porttitor ex vitae, sollicitudin lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent vel metus porttitor elit tincidunt rutrum eget a leo. Nulla facilisi. Praesent ut sollicitudin mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in eleifend ipsum. Duis hendrerit turpis et sapien hendrerit, et convallis ligula ultrices. Integer venenatis venenatis neque non feugiat. Maecenas pretium nisi a pharetra malesuada. Sed bibendum lorem eu elit mattis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed fermentum dapibus nibh, eget commodo ante porttitor ut. Suspendisse pulvinar, magna id pulvinar mattis, tellus purus faucibus lorem, non porttitor neque massa quis odio. Duis lobortis suscipit nunc, id consectetur nisl vestibulum at. Curabitur id dui lacinia, porttitor ex vitae, sollicitudin lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent vel metus porttitor elit tincidunt rutrum eget a leo. Nulla facilisi. Praesent ut sollicitudin mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in eleifend ipsum. Duis hendrerit turpis et sapien hendrerit, et convallis ligula ultrices. Integer venenatis venenatis neque non feugiat. Maecenas pretium nisi a pharetra malesuada. Sed bibendum lorem eu elit mattis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed fermentum dapibus nibh, eget commodo ante porttitor ut. Suspendisse pulvinar, magna id pulvinar mattis, tellus purus faucibus lorem, non porttitor neque massa quis odio. Duis lobortis suscipit nunc, id consectetur nisl vestibulum at. Curabitur id dui lacinia, porttitor ex vitae, sollicitudin lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent vel metus porttitor elit tincidunt rutrum eget a leo. Nulla facilisi. Praesent ut sollicitudin mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+          </Block>
+        </div>
         <Map />
       </div>
     );
