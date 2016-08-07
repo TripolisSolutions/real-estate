561aadf1610d90ceb76137103882962366fb41b5
Tinh Ngo <tiengtinh@gmail.com>
2016-08-07 08:35:13 +0700

    setup table

--- src/app/containers/Admin/PropertiesList/PropertiesList.tsx
+++ src/app/containers/Admin/PropertiesList/PropertiesList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as log from 'loglevel'
 const { connect } = require('react-redux');
 const { asyncConnect } = require('redux-connect');
 
@@ -8,7 +9,15 @@ import { IState } from '../../../redux/reducers'
 
 // const s = require('./PropertiesList.less')
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { IndexLink } from 'react-router'
+
 interface IProps extends IState {
+}
+
+function linkFormatter(cell, row) {
+  log.debug('linkFormatter cell: ', cell, ' row: ', row)
+  return <IndexLink to={ `/properties/${ row.id }` } target='_blank'></IndexLink>
 }
 
 @asyncConnect([{
@@ -27,7 +36,13 @@ class PropertiesList extends React.Component<IProps, {}> {
   public render() {
     return(
       <div>
-        { this.props.categoriesData.isFetching ? 'Fetching Stars' : 'fetched' }
+        { this.props.propertiesData.isFetching ? 'Fetching Properties' : (
+          <BootstrapTable data={ this.props.propertiesData.properties }>
+            <TableHeaderColumn dataField='name' dataSort={true}>Title</TableHeaderColumn>
+            <TableHeaderColumn dataField='c_at' dataSort={true}>Created Date</TableHeaderColumn>
+            <TableHeaderColumn dataField='id' isKey={true} dataFormat={ linkFormatter }>Link</TableHeaderColumn>
+          </BootstrapTable>
+        ) }
       </div>
     );
   }
