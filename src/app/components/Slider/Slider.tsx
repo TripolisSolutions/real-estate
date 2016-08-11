d09ba31301461c9ca696a37fa6f43fd39892fa8c
lam.khuong <lam.khuong@tripolis.com>
2016-08-11 18:47:27 +0700

    fix hover list add slider detail page

--- src/app/components/Slider/Slider.tsx
+++ src/app/components/Slider/Slider.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react'
-import ImageGallery from 'react-image-gallery'
+import { Carousel } from 'react-bootstrap'
 
-import 'react-image-gallery/build/image-gallery.css'
+const s = require('./Slider.less')
 
 class Slider extends React.Component<any, any> {
 
   public render() {
     return(
-      <div className={'container'}>
-        <ImageGallery showThumbnails={false}
-          items= { [
-            {original:'http://www.pixelstalk.net/wp-content/uploads/2016/05/Laptop-Wallpapers-HD-For-Windows-10-HD.jpg'},
-            {original:'http://www.pixelstalk.net/wp-content/uploads/2016/05/Laptop-Wallpapers-HD-For-Windows-10-HD-Download.jpg'}
-          ] }
-        />
+      <div className={ s.container }>
+        <div className={ s.box }>
+          <span>Nice to meet you</span>
+        </div>
+        <Carousel className={ s.slider }>
+          <Carousel.Item>
+            <img width='100%' alt='900x500' src='http://ghk.h-cdn.co/assets/cm/15/11/54ff82282ac26-living-room-green-window-de.jpg'/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width='100%' alt='900x500' src='http://www.uum.org.my/wp-content/uploads/2016/05/s-ac2562875c06eae6cf546b0c0cf10b6f47311b47.jpg'/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width='100%' alt='900x500' src='http://www.beeyoutifullife.com/wp-content/uploads/2014/12/mid-century-modern-rugs-Living-Room-Modern-with-none-.jpg'/>
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
