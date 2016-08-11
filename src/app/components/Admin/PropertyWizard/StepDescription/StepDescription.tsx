7063cc9dd84fa931ab03aebb121dca8a831a1a5d
Tinh Ngo <tiengtinh@gmail.com>
2016-08-11 20:53:28 +0700

    react quill as rich editor

--- src/app/components/Admin/PropertyWizard/StepDescription/StepDescription.tsx
+++ src/app/components/Admin/PropertyWizard/StepDescription/StepDescription.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 import { translate, InjectedTranslateProps } from 'react-i18next'
 import * as log from 'loglevel'
 
-const AlloyEditor = require('../../../alloyeditor/dist/alloy-editor/alloy-editor-no-react')
-require('../../../alloyeditor/dist/alloy-editor/assets/alloy-editor-ocean.css')
+const ReactQuill = require('react-quill')
 
+require('./quill.snow.less')
 const s = require('./StepDescription.less')
 
 interface IProps extends InjectedTranslateProps {
@@ -15,46 +15,18 @@ interface IProps extends InjectedTranslateProps {
 
 class StepDescription extends React.Component<IProps, void> {
 
-  private id: string
-  private retrieveContentInterval
-  private editor
-
   constructor(props: IProps) {
     super(props)
-
-    this.id = Math.random().toString()
-
-    let prevContent
-    this.retrieveContentInterval = setInterval(() => {
-      const content = this.editor.get('nativeEditor').getData()
-
-      if (prevContent === content) {
-        return
-      }
-
-      log.debug('interval saving content: ', content)
-      props.onChange(content)
-      prevContent = content
-    }, 1000)
-  }
-
-  public componentDidMount() {
-    log.debug('RichEditor id: ', this.id)
-    this.editor = AlloyEditor.editable(this.id, {})
-  }
-
-  public componentWillUnmount() {
-    this.editor.destroy()
-    clearInterval(this.retrieveContentInterval)
   }
 
   public render() {
     const { t } = this.props
 
     return (
-      <div id={ this.id } className={ s.editor }>
-        <p>{ this.props.initialValue || t('description_default_content')  }</p>
-      </div>
+      <ReactQuill theme='snow'
+        value={ this.props.initialValue }
+        onChange={this.props.onChange}
+      />
     )
   }
 }
