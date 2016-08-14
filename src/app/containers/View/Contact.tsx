389ba628bf2522e425a9c20941eb5eb10107e168
Tinh Ngo <tiengtinh@gmail.com>
2016-08-14 15:12:22 +0700

    stateful contact

--- src/app/containers/View/Contact.tsx
+++ src/app/containers/View/Contact.tsx
@@ -1,25 +1,45 @@
 import * as React from 'react'
 import { translate, InjectedTranslateProps } from 'react-i18next'
 
+const { connect } = require('react-redux')
+
 import Block from '../../components/Block/Block'
 import LocationMap from '../../components/LocationMap/LocationMap'
 import ContactInput from '../../components/ContactInput/ContactInput'
 
 interface IProps extends InjectedTranslateProps {
-
+  sendContactRequest: Redux.ActionCreator
 }
 
-const Contact = (props: IProps) => {
-  return (
-    <div>
-      <div className={ 'container' } >
-        <Block title={ props.t('contact_us') } bigger ={ true }>
-          <ContactInput />
-        </Block>
-      </div>
-      <LocationMap />
-    </div>
-  )
+interface IState {
+  isFetching: boolean
+}
+
+class Contact extends React.Component<IProps, IState> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isFetching: false,
+    }
   }
 
+  public render() {
+    const { props } = this
+
+    return (
+      <div>
+        <div className={ 'container' } >
+          <Block title={ props.t('contact_us') } bigger ={ true }>
+            <ContactInput
+              lock={ this.state.isFetching }
+              onSubmit={ props.sendContactRequest() }
+            />
+          </Block>
+        </div>
+        <LocationMap />
+      </div>
+    )
+  }
+}
 export default translate()(Contact)
